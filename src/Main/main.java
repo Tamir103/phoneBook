ee059fee8907ffb86fe60c80e983418124596a85
Tamir103 <tamir.markman@gmail.com>
2022-10-11 13:09:25 +0300

    name validation fixes

--- src/Main/main.java
+++ src/Main/main.java
@@ -6,7 +6,9 @@ public class main {
     public static void main(String[] args) {
 
         contact p1 = new contact();
-        p1.setName("2");
+        p1.setName("     ");
+
+//        p1.setPhoneNumber("054-791");
 
     }
 }
--- src/utils/contact.java
+++ src/utils/contact.java
@@ -1,18 +1,36 @@
 package utils;
 
+/**
+ * Phone book contact object.
+ * Contains name and phone number for each contact object
+ */
 public class contact {
     private String name;
     private String phoneNumber;
 
     public void setName(String name) {
         // ASCII letters a = 97, z = 122, A = 65, Z = 90
-        for (int i = 0; i < name.length(); i++) {
-            char currentChar = name.charAt(i);
-            int charValue = (int) currentChar;
-            if (!((charValue >= 65 && charValue <= 90) || (charValue >= 97 && charValue <= 122))) {
-                throw new IllegalArgumentException("ONLY ENGLISH LETTERS ALLOWED");
+        name.trim();
+        if (name.length() > 0) {
+            for (int i = 0; i < name.length(); i++) {
+                char currentChar = name.charAt(i);
+                int charValue = (int) currentChar;
+                if (!((charValue >= 65 && charValue <= 90) || (charValue >= 97 && charValue <= 122))) {
+                    throw new IllegalArgumentException("ONLY ENGLISH LETTERS ALLOWED");
+                }
             }
+            System.out.println(this.name);
+            this.name = name;
+        } else {
+            throw new IllegalArgumentException("Name attribute is empty");
         }
-        this.name = name;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+
+        int phoneNumberInt = Integer.parseInt(phoneNumber);
     }
 }
