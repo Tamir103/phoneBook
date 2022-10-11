16873920e6c7362f3909fa7600b34a5757c9e136
Tamir103 <tamir.markman@gmail.com>
2022-10-11 12:45:33 +0300

    name validation

---
+++ src/Main/main.java
@@ -0,0 +1,12 @@
+package Main;
+
+import utils.contact;
+
+public class main {
+    public static void main(String[] args) {
+
+        contact p1 = new contact();
+        p1.setName("2");
+
+    }
+}
--- src/utils/contact.java
+++ src/utils/contact.java
@@ -1,8 +1,18 @@
 package utils;
 
 public class contact {
-    String name;
-    String phoneNumber;
+    private String name;
+    private String phoneNumber;
 
-
+    public void setName(String name) {
+        // ASCII letters a = 97, z = 122, A = 65, Z = 90
+        for (int i = 0; i < name.length(); i++) {
+            char currentChar = name.charAt(i);
+            int charValue = (int) currentChar;
+            if (!((charValue >= 65 && charValue <= 90) || (charValue >= 97 && charValue <= 122))) {
+                throw new IllegalArgumentException("ONLY ENGLISH LETTERS ALLOWED");
+            }
+        }
+        this.name = name;
+    }
 }
